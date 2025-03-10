e73e913a5f7432ae91103e7c8ee7a80855f96ae8
hajime_kuno <hajimekuno10@gmail.com>
2025-03-11 00:01:15 +0900

    add README

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # zero_inflated_bayesian_ab
+
+## Overview
+
+This package provides a Bayesian approach for conducting AB tests on data following a Zero-Inflated Lognormal distribution. It enables users to analyze revenue per user when the data contains a significant number of zeros.
+
+## Model Description
+
+This package assumes that the observed values follow a Zero-Inflated Lognormal distribution. Since both Group A and Group B share the same distributional structure, we describe the model for Group A:
+
+```math
+y_A \sim (1-\pi_A)\delta(y_A) + \pi_A \mathcal{LN}(\mu_A, \sigma_A^2)
+```
+
+where $\delta(y)$ is the Dirac delta function, and $\mathcal{LN}(\mu, \sigma^2)$ is the probability density function of a lognormal distribution. The prior distributions for the parameters are assumed as follows:
+
+```math
+\begin{align*}
+\pi_A &\sim \mathcal{B}(\alpha_A, \beta_A) \\
+\mu_A &\sim \mathcal{N}(\theta_A, \sigma_A^2 / \lambda_A) \\
+\sigma_A^2 &\sim \mathcal{IG}(a_A, b_A)
+\end{align*}
+```
+
+where $\mathcal{B}(\alpha_A, \beta_A)$ denotes a Beta distribution, $\mathcal{N}(\theta, \sigma^2)$ represents a Normal distribution, and $mathcal{IG}(a, b)$ is an Inverse Gamma distribution.
+
+## Installation
+
+```bash
+!pip install git+https://github.com/Kuno3/zero_inflated_bayesian_ab
+```
+
+## Usage
+
+```python
+from zero_inflated_bayesian_ab.zero_inflated_bayesian_ab import (
+    zero_inflated_bayesian_ab,
+)
+
+# Example Data (Group A and Group B)
+model = zero_inflated_bayesian_ab(data_A, data_B)
+# Run Sampling
+model.sampling(100000)
+# Summary plot
+model.summary()
+```
+
+## API Reference
+
+### `zero_inflated_bayesian_ab(data_A, data_B, alpha_A,  alpha_B, beta_A, beta_B, theta_A, theta_B, lambda_A, lambda_B, a_A, a_B, b_A, b_B))`
+
+- **data_A**: List or numpy array of observations from group A.
+- **data_B**: List or numpy array of observations from group B.
+- **, alpha_A, alpha_B, beta_A, beta_B, theta_A, theta_B, lambda_A, lambda_B, a_A, a_B, b_A, b_B**: (Optional) Parameters for priors.
+- **Returns**: An object containing posterior estimates and visualization methods.
+
+### `sampling(num_samples)`
+
+- **num_samples**: Number of samples.
+
+### `summary()`
+
+- Generates plots for posterior distributions.
+
+## Dependencies
+
+- `numpy`
+- `scipy`
+- `matplotlib`
+
+## License
+
+MIT License
--- zero_inflated_bayesian_ab/zero_inflated_bayesian_ab.py
+++ zero_inflated_bayesian_ab/zero_inflated_bayesian_ab.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import beta, bernoulli, normal_inverse_gamma, norm, lognorm
+from scipy.stats import beta, bernoulli, normal_inverse_gamma, norm
 import matplotlib.pyplot as plt
 
 
@@ -8,33 +8,33 @@ class zero_inflated_bayesian_ab:
         self,
         data_A,
         data_B,
-        pi_A_prior_param_1=1,
-        pi_B_prior_param_1=1,
-        pi_A_prior_param_2=1,
-        pi_B_prior_param_2=1,
-        mu_A_prior_param_1=0,
-        mu_B_prior_param_1=0,
-        mu_A_prior_param_2=100,
-        mu_B_prior_param_2=100,
-        sigma_A_prior_param_1=1,
-        sigma_B_prior_param_1=1,
-        sigma_A_prior_param_2=1,
-        sigma_B_prior_param_2=1,
+        alpha_A=1,
+        alpha_B=1,
+        beta_A=1,
+        beta_B=1,
+        theta_A=0,
+        theta_B=0,
+        lambda_A=100,
+        lambda_B=100,
+        a_A=1,
+        a_B=1,
+        b_A=1,
+        b_B=1,
     ):
         self.data_A = data_A
         self.data_B = data_B
-        self.pi_A_prior_param_1 = pi_A_prior_param_1
-        self.pi_B_prior_param_1 = pi_B_prior_param_1
-        self.pi_A_prior_param_2 = pi_A_prior_param_2
-        self.pi_B_prior_param_2 = pi_B_prior_param_2
-        self.mu_A_prior_param_1 = mu_A_prior_param_1
-        self.mu_B_prior_param_1 = mu_B_prior_param_1
-        self.mu_A_prior_param_2 = mu_A_prior_param_2
-        self.mu_B_prior_param_2 = mu_B_prior_param_2
-        self.sigma_A_prior_param_1 = sigma_A_prior_param_1
-        self.sigma_B_prior_param_1 = sigma_B_prior_param_1
-        self.sigma_A_prior_param_2 = sigma_A_prior_param_2
-        self.sigma_B_prior_param_2 = sigma_B_prior_param_2
+        self.alpha_A = alpha_A
+        self.alpha_B = alpha_B
+        self.beta_A = beta_A
+        self.beta_B = beta_B
+        self.theta_A = theta_A
+        self.theta_B = theta_B
+        self.lambda_A = lambda_A
+        self.lambda_B = lambda_B
+        self.a_A = a_A
+        self.a_B = a_B
+        self.b_A = b_A
+        self.b_B = b_B
         self.pi_A_samples = None
         self.pi_B_samples = None
         self.mu_A_samples = None
@@ -51,70 +51,69 @@ class zero_inflated_bayesian_ab:
         log_data_non_zero_A = np.log(data_A[data_A > 0])
         log_data_non_zero_B = np.log(data_B[data_B > 0])
 
-        self.pi_A_posterior_param_1 = self.pi_A_prior_param_1 + n_non_zero_A
-        self.pi_B_posterior_param_1 = self.pi_B_prior_param_1 + n_non_zero_B
-        self.pi_A_posterior_param_2 = self.pi_A_prior_param_2 + n_A - n_non_zero_A
-        self.pi_B_posterior_param_2 = self.pi_B_prior_param_2 + n_B - n_non_zero_B
-        self.mu_A_posterior_param_1 = (
-            self.mu_A_prior_param_2 * self.mu_A_prior_param_1
-            + log_data_non_zero_A.sum()
-        ) / (self.mu_A_prior_param_2 + n_non_zero_A)
-        self.mu_B_posterior_param_1 = (
-            self.mu_B_prior_param_2 * self.mu_B_prior_param_1
-            + log_data_non_zero_B.sum()
-        ) / (self.mu_B_prior_param_2 + n_non_zero_B)
-        self.mu_A_posterior_param_2 = self.mu_A_prior_param_2 + n_non_zero_A
-        self.mu_B_posterior_param_2 = self.mu_B_prior_param_2 + n_non_zero_B
-        self.sigma_A_posterior_param_1 = self.sigma_A_prior_param_1 + n_non_zero_A / 2
-        self.sigma_B_posterior_param_1 = self.sigma_B_prior_param_1 + n_non_zero_B / 2
-        self.sigma_A_posterior_param_2 = (
-            self.sigma_A_prior_param_2
+        self.posterior_alpha_A = self.alpha_A + n_non_zero_A
+        self.posterior_alpha_B = self.alpha_B + n_non_zero_B
+        self.posterior_beta_A = self.beta_A + n_A - n_non_zero_A
+        self.posterior_beta_B = self.beta_B + n_B - n_non_zero_B
+        self.posterior_theta_A = (
+            self.lambda_A * self.theta_A + log_data_non_zero_A.sum()
+        ) / (self.lambda_A + n_non_zero_A)
+        self.posterior_theta_B = (
+            self.lambda_B * self.theta_B + log_data_non_zero_B.sum()
+        ) / (self.lambda_B + n_non_zero_B)
+        self.posterior_lambda_A = self.lambda_A + n_non_zero_A
+        self.posterior_lambda_B = self.lambda_B + n_non_zero_B
+        self.posterior_a_A = self.a_A + n_non_zero_A / 2
+        self.posterior_a_A = self.a_B + n_non_zero_B / 2
+        self.posterior_b_A = (
+            self.b_A
             + (
                 n_non_zero_A * log_data_non_zero_A.var()
                 + (
-                    self.mu_A_prior_param_2
+                    self.lambda_A
                     * n_non_zero_A
-                    * (log_data_non_zero_A.mean() - self.mu_A_prior_param_1) ** 2
+                    * (log_data_non_zero_A.mean() - self.theta_A) ** 2
                 )
-                / (self.mu_A_prior_param_2 + n_non_zero_A)
+                / (self.lambda_A + n_non_zero_A)
             )
             / 2
         )
-        self.sigma_B_posterior_param_2 = (
-            self.sigma_B_prior_param_2
+        self.posterior_a_B = (
+            self.b_B
             + (
                 n_non_zero_B * log_data_non_zero_B.var()
                 + (
-                    self.mu_B_prior_param_2
+                    self.lambda_B
                     * n_non_zero_B
-                    * (log_data_non_zero_B.mean() - self.mu_B_prior_param_1) ** 2
+                    * (log_data_non_zero_B.mean() - self.theta_B) ** 2
                 )
-                / (self.mu_B_prior_param_2 + n_non_zero_B)
+                / (self.lambda_B + n_non_zero_B)
             )
             / 2
         )
 
-    def sampling(self, num_samples=100000):
-        self.pi_A_samples = beta(
-            self.pi_A_posterior_param_1, self.pi_A_posterior_param_2
-        ).rvs(num_samples)
-        self.pi_B_samples = beta(
-            self.pi_B_posterior_param_1, self.pi_B_posterior_param_2
-        ).rvs(num_samples)
+    def sampling(self, num_samples=100000, seed=1234):
+        np.random.seed(seed)
+        self.pi_A_samples = beta(self.posterior_alpha_A, self.posterior_beta_A).rvs(
+            num_samples
+        )
+        self.pi_B_samples = beta(self.posterior_alpha_B, self.posterior_beta_B).rvs(
+            num_samples
+        )
         delta_A_samples = bernoulli(self.pi_A_samples).rvs()
         delta_B_samples = bernoulli(self.pi_B_samples).rvs()
 
         self.mu_A_samples, self.sigma_A_samples = normal_inverse_gamma(
-            self.mu_A_posterior_param_1,
-            self.mu_A_posterior_param_2,
-            self.sigma_A_posterior_param_1,
-            self.sigma_A_posterior_param_2,
+            self.posterior_theta_A,
+            self.posterior_lambda_A,
+            self.posterior_a_A,
+            self.posterior_b_A,
         ).rvs(num_samples)
         self.mu_B_samples, self.sigma_B_samples = normal_inverse_gamma(
-            self.mu_B_posterior_param_1,
-            self.mu_B_posterior_param_2,
-            self.sigma_B_posterior_param_1,
-            self.sigma_B_posterior_param_2,
+            self.posterior_theta_B,
+            self.posterior_lambda_B,
+            self.posterior_a_A,
+            self.posterior_a_B,
         ).rvs(num_samples)
 
         self.tilde_Y_A_samples = np.exp(
@@ -129,6 +128,7 @@ class zero_inflated_bayesian_ab:
     def summary(self):
         if self.Y_A_samples is None:
             print("Model has not been sampled yet.")
+
         fig, ax = plt.subplots(3, 1, figsize=(6, 12))
 
         probability_Y_A_over_Y_B = round(
@@ -142,12 +142,12 @@ class zero_inflated_bayesian_ab:
         x = np.arange(0, 1, 0.001)
         ax[1].plot(
             x,
-            beta(self.pi_A_posterior_param_1, self.pi_A_posterior_param_2).pdf(x),
+            beta(self.posterior_alpha_A, self.posterior_beta_A).pdf(x),
             label="group A",
         )
         ax[1].plot(
             x,
-            beta(self.pi_B_posterior_param_1, self.pi_B_posterior_param_2).pdf(x),
+            beta(self.posterior_alpha_B, self.posterior_beta_B).pdf(x),
             label="group B",
         )
         ax[1].set_title("Probability of non-zero")
